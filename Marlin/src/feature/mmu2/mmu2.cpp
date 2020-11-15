3c52e21daf4ac4ff56a405632fd2f42dc154ec11
Nicco <niccoreyes@gmail.com>
2020-11-15 17:23:49 +0800

    Cleaned spacing in VScode & removed U0 in toolchange
    I commented out the  command(MMU_CMD_U0);
    in the tool_change() since I checked the MMU code does manage whether to do an unload/not with the succeeding "command(MMU_CMD_T0 + index);" if the currently assigned extruder has changed.
    
    so as not to sound confusing, the goal of keeping the extruder cleared with any filament in the gears is still there, I just removed an excess MMU command that is already managed by the succeeding command anyway.

--- Marlin/src/feature/mmu2/mmu2.cpp
+++ Marlin/src/feature/mmu2/mmu2.cpp
@@ -513,22 +513,23 @@ static void mmu2_not_responding() {
     set_runout_valid(false);
 
     if (index != extruder) {
-        #if ENABLED(MMU_IR_UNLOAD_MOVE)
-		  if(FILAMENT_PRESENT()){
-			DEBUG_ECHOLNPGM("Unloading\n");
-			//filament_ramming(); //unloading from printer side
-			ENABLE_AXIS_E0();
-			while (FILAMENT_PRESENT()){ //if filament is still present, keep unloading
-			  current_position.e -=0.25;
-			  line_to_current_position(MMM_TO_MMS(120));
-			  planner.synchronize();
-			}
-			DISABLE_AXIS_E0(); //disable E stepper when unloading finished
-			command(MMU_CMD_U0); //unloading from mmu side but not printer
-			manage_response(true,true);
-		  }
+      #if ENABLED(MMU_IR_UNLOAD_MOVE)
+		    if(FILAMENT_PRESENT()){
+			    DEBUG_ECHOLNPGM("Unloading\n");
+			    //filament_ramming(); //unloading from printer side
+			    ENABLE_AXIS_E0();
+			    while (FILAMENT_PRESENT()){ //if filament is still present, keep unloading
+			      current_position.e -=0.25; //this is a guessed value, adjust to your preference
+			      line_to_current_position(MMM_TO_MMS(120));
+			      planner.synchronize();
+			    }
+			    DISABLE_AXIS_E0(); //disable E stepper when unloading finished
+			    //command(MMU_CMD_U0); //unloading from mmu side but not printer
+          // ^commented out because command(MMU_CMD_T0 + index); manages the current loaded tool
+			    manage_response(true,true);
+		    }
 		  else { //filament was unloaded from idler, no additional movements needed 
-			DISABLE_AXIS_E0();
+			  DISABLE_AXIS_E0();
 		  }
 		#else // !MMU_IR_UNLOAD_MOVE - default
 			DISABLE_AXIS_E0();
@@ -954,23 +955,24 @@ void MMU2::filament_runout() {
 	#if ENABLED(MMU_IR_UNLOAD_MOVE)
 		// custom THIS IS FOR LOAD TO NOZZLE FUNCTION ONLY
 		if (index != extruder){
-		  
 		  if(FILAMENT_PRESENT()){
-			DEBUG_ECHOLNPGM("Unloading\n");        
-			ENABLE_AXIS_E0();
-			filament_ramming(); //unloading from printer side ! EXCLUSIVE FOR THIS FUNCTION
-			while (FILAMENT_PRESENT()){ // if filament is still present, keep unloading - WARNING I'm not sure 
-										// if this is a recommended coding style in marlin
-			  current_position.e -=1;
-			  line_to_current_position(MMM_TO_MMS(120));
-			  planner.synchronize();
-			}
-			DISABLE_AXIS_E0(); //disable E stepper when unloading finished
-			command(MMU_CMD_U0); //unloading from mmu side but not printer
-			manage_response(true,true);
+			  DEBUG_ECHOLNPGM("Unloading\n");        
+			  ENABLE_AXIS_E0();
+			  filament_ramming(); //unloading from printer side ! EXCLUSIVE FOR THIS FUNCTION
+			  while (FILAMENT_PRESENT()){ // if filament is still present, keep unloading - WARNING I'm not sure 
+										                // if this is a recommended coding style in marlin
+			    current_position.e -=1; //this is a guessed value
+			    line_to_current_position(MMM_TO_MMS(120));
+			    planner.synchronize();
+			  }
+			  DISABLE_AXIS_E0(); //disable E stepper when unloading finished
+			  command(MMU_CMD_U0); //unloading from mmu side but not printer
+        // ^ needed because if startup, printer might not know current tool loaded
+        // this helps flag both mmu to know nothing is loaded
+		  	manage_response(true,true);
 		  }
 		  else { //filament was unloaded from idler, no additional movements needed 
-			DISABLE_AXIS_E0(); 
+			  DISABLE_AXIS_E0(); 
 		  }
 		}
 	#endif // MMU_IR_UNLOAD_MOVE
